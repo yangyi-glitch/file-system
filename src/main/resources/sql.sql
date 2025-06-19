b528c371feed58a0c02f74a1672da51d9f5c765d
yangyi <1965997927@qq.com>
2025-06-19 10:04:40 +0800

    feat(database): 新增用户和文章表并添加相关数据
    - 新增 user 表，用于存储用户信息
    - 新增 article 表，用于存储文章信息
    -向 user 表中插入几个示例用户数据
    - 优化 file 表的创建语句，使其格式更加规范

--- src/main/resources/sql.sql
+++ src/main/resources/sql.sql
@@ -1,7 +1,35 @@
-CREATE TABLE `file` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `file_name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `file_url` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `file_type` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `file`
+(
+    `id`        int(11) NOT NULL AUTO_INCREMENT,
+    `file_name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+    `file_url`  varchar(255) COLLATE utf8_bin DEFAULT NULL,
+    `file_type` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE `user`
+(
+    `user_id`       int(11) NOT NULL AUTO_INCREMENT,
+    `account`       varchar(255) COLLATE utf8_bin DEFAULT NULL,
+    `username`      varchar(255) COLLATE utf8_bin DEFAULT NULL,
+    `password`      varchar(255) COLLATE utf8_bin DEFAULT NULL,
+    `email`         varchar(255) COLLATE utf8_bin DEFAULT NULL,
+    `created`       varchar(255) COLLATE utf8_bin DEFAULT NULL,
+    `last_modified` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+    `role`          int(255) DEFAULT NULL,
+    PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+INSERT INTO `user` VALUES (1, '8000843017', '杨溢', '123456', '1965997927@qq.com', '2024-07-18', '2024-07-18', 0);
+INSERT INTO `user` VALUES (2, '1067307503', '压索', '123456', '1965997927@qq.com', '2024-07-18', '2024-07-18', 1);
+INSERT INTO `user` VALUES (4, '12322534', '小智', '123456', '42143512', '2024-09-19', '2024-09-19', 1);
+INSERT INTO `user` VALUES (5, 'admin', 'admin', 'admin', '1965997927@qq.com', '2024-09-19', '2024-09-19', 0);
+
+CREATE TABLE `article`
+(
+    `id`        int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `book_name` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '书名',
+    `price`     varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '价格',
+    `author`    varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '作者',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
